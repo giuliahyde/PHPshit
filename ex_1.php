6f0186beeee36bf4fd952d2c23ad603fa4a9b8e1
Andrea Leone <andrea@etrentino.it>
2017-03-28 17:01:51 +0200

    correzione e rinomina

---
+++ ex_1.php
@@ -0,0 +1,28 @@
+<html>
+	<head>
+		<title>Somma di 2 numeri</title>
+	</head>
+
+	<body>
+
+		<h2>Somma di due numeri</h2>
+
+		<form name="form" method="post" action="Esercizio4Somma.php">
+			Primo addendo <input type="text" name="Add1" value="0" maxlength="5">
+			<br>
+			Secondo addendo <input type="text" name="Add2" value="0" maxlength="5">
+			<br>
+			Totale: <input type="submit" value="Somma">
+		</form>
+
+		<?php
+
+			if ( isset($_POST['Add1']) && isset($_POST['Add2']) ){
+				$num1 = $_POST['Add1'];
+				$num2 = $_POST['Add2'];
+				echo "la somma di $num1 + $num2 = " . ($num1 + $num2);
+			}
+
+		?>
+	</body>
+</html>
